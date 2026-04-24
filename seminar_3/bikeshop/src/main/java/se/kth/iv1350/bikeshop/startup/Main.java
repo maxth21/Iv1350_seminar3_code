21997acf464efc3cc63f1034f0bce0fb0b160830
Nipun Thianwan <160643608+maxth21@users.noreply.github.com>
2026-04-24 14:10:55 +0200

    Create Main class with main method for application
    Fix import statements and correct class and method names.

---
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/startup/Main.java
@@ -0,0 +1,18 @@
+package se.kth.iv1350.bikeshop.startup;
+
+imort se.kth.iv1350.bikeshop.controller.Controller;
+imort se.kth.iv1350.bikeshop.integration.Printer;
+imort se.kth.iv1350.bikeshop.integration.RegistryCreator;
+imort se.kth.iv1350.bikeshop.view.View;
+
+public main {
+  /**
+    * Contains the main method - start the application.
+    */
+  public static void main(String[] args) {
+    ReigistryCreator registry = new RegistryCreator();
+    Printer printer = new Printer();
+    Controller controller = new Controller(registry, printer);
+    View.startRepairShop();
+  }
+}
