16d0811848e40aca20c514013c0d7ce3432d4e8f
Zainab Noah <zainabn@kth.se>
2026-04-28 14:42:37 +0200

    added CustomerDTO

---
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/dto/CustomerDTO.java
@@ -0,0 +1,31 @@
+package se.kth.iv1350.bikeshop.dto;
+import java.util.List;
+/**
+ * DTO with details about customer
+ */
+
+public class CustomerDTO{
+
+    private String name;
+    private String phoneNr;
+    private String email;
+
+    public CustomerDTO(String name, String phoneNr, String email){
+        this.name = name;
+        this.phoneNr = phoneNr;
+        this.email = email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNr(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+}
