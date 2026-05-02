42c672ecfc9d150034415dfbafa15def9051f144
jeka-0111 <jeka7393@gmail.com>
2026-05-02 10:10:38 +0200

    added hardcoded values in RepOrderDTO, added comments and minor changes

--- seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/dto/RepairOrderDTO.java
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/dto/RepairOrderDTO.java
@@ -10,15 +10,23 @@ import java.util.List;      // List = interface type (good practice: program to 
  */
 public class RepairOrderDTO {
 
+    /**
+     * Hard-coded value for date in order to test the program
+    */
+    public final int CURRENT_DATE = 260504;
+
     private final String repairOrderId;        // final = ID never changes after creation
     private final String problemDescription;    // final = problem description never changes
-    private final int date;
-    private int EstimatedCompletionDate;
+    private final int date = CURRENT_DATE;
+    private int estimatedCompletionDate;
     private double totalCost;                   // NOT final = changes when repair tasks are added
     private boolean accepted;                       // NOT final = changes when order is accepted/rejected
    
     /**
      * Creates a new instance with the specified repair order data.
+     * Initializes boolean accepted as false.
+     * Uses CURRENT_DATE as hardcoded value so that we cannot change the date after the object has been created.
+     * 
      *
      * @param repairOrderId      The unique ID of the repair order.
      * @param 
@@ -27,11 +35,11 @@ public class RepairOrderDTO {
      * @param accepted           The current state of the repair order.
      */
     public RepairOrderDTO(String repairOrderId, CustomerDTO customer, BikeDTO bike,
-                          String problemDescription, double totalCost, String accepted) {
+                          String problemDescription, double totalCost, boolean accepted) {
         this.repairOrderId = repairOrderId;         // Store the unique order ID
         this.problemDescription = problemDescription; // Store the reported problem
         this.totalCost = totalCost;                  // Store initial cost (usually 0.0 at creation)
-        this.accepted = accepted;                          // Store initial state (e.g. "created")
+        this.accepted = false;                          // Store initial state (e.g. "created")
     }
 
     /** @return The unique repair order ID. */
@@ -44,7 +52,7 @@ public class RepairOrderDTO {
         return problemDescription;
     }
     
-    /** @return The total cost of all repair tasks. */
+    /** @return The date. */
     public int getDate() {
         return date;
     }
@@ -59,7 +67,7 @@ public class RepairOrderDTO {
         return estimatedCompletionDate;
     }
     /** @return The current state of the repair order. */
-    public String getAccepted() {
+    public boolean getAccepted() {
         return accepted;
     }
 
