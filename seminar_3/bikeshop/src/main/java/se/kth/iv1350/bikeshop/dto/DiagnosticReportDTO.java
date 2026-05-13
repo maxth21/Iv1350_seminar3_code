d1b2c95fc0136e222486c9ac3372efe7f8a051fd
Zainab Noah <zainabn@kth.se>
2026-05-13 17:10:01 +0200

    changed the name to diagnosticReportProblemDescription

--- seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/dto/DiagnosticReportDTO.java
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/dto/DiagnosticReportDTO.java
@@ -4,23 +4,26 @@ package se.kth.iv1350.bikeshop.dto;
  * DTO for the DiagnosticReport
  */
 
+
 public class DiagnosticReportDTO {
-    private String problemDescription;
+    private String diagnosticReportProblemDescription;
+
+    //what tasks to be fixed.
 
     /**
      * Constructor for the diagnostic report dto
      * @param problemDescription description of problem with the bike.
      */
 
-    public DiagnosticReportDTO(String problemDescription){
-        this.problemDescription = problemDescription;
+    public DiagnosticReportDTO(String diagnosticReportProblemDescription){
+        this.diagnosticReportProblemDescription = diagnosticReportProblemDescription;
     }
 
     /**
      * Get method for problem description
      * @return the description of the problem with the bike
      */
-    public String getProblemDescription() {
-        return problemDescription;
+    public String getDiagnosticReportProblemDescription() {
+        return diagnosticReportProblemDescription;
     }   
 }
