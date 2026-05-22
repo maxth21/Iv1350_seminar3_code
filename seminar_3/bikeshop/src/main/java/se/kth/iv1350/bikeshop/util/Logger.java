db4021783e00593a5289b45d3c8de0a6a9d177b8
jeka-0111 <jeka7393@gmail.com>
2026-05-22 22:41:04 +0200

    logger files

---
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/util/Logger.java
@@ -0,0 +1,12 @@
+package se.kth.iv1350.bikeshop.util;
+
+public interface Logger {
+    
+    /**
+     * creates an object tha can be printed to a log
+     * 
+     */
+        void log(String messageString);
+}
+
+
