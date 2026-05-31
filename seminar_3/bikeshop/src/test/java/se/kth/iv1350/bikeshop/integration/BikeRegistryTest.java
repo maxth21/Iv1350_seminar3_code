3f673600669d74d9ad1d593738dd8bada048db93
Zainab Noah <zainabn@kth.se>
2026-05-31 23:49:09 +0200

    Added testmethods

--- seminar_3/bikeshop/src/test/java/se/kth/iv1350/bikeshop/integration/BikeRegistryTest.java
+++ seminar_3/bikeshop/src/test/java/se/kth/iv1350/bikeshop/integration/BikeRegistryTest.java
@@ -1,25 +1,36 @@
 package se.kth.iv1350.bikeshop.integration;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class BikeRegistryTest {
-    
-    private CustomerRegistry registry;
-    
+
+    private BikeRegistry registry;
+
     @BeforeEach
     public void setUp() {
-        registry = new CustomerRegistry();
+        registry = new BikeRegistry();
     }
-    
-    @Test
-    public void test (){
-      
-    }
- 
 
+    @Test
+    public void testIfObjectIsCreated() {
+        assertNotNull(registry, "BikeRegistry object should be created.");
+    }
+
+    @Test
+    public void testFindExistingBikeReturnsNotNull() {
+        assertNotNull(registry.findBike("0701234567"),
+                "findBike should return a BikeDTO for a known phone number.");
+    }
+
+    @Test
+    public void testFindBikeWithUnknownPhoneNumberReturnsNull() {
+        assertNull(registry.findBike("0000000000"),
+                "findBike should return null for an unknown phone number.");
+    }
 
     @AfterEach
     void tearDown() {
